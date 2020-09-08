d878b2153ce4d1093f6d40e734ae0e3b96b55042
Parth Patel <parthpatel1105@gmail.com>
2020-09-08 19:28:15 -0400

    First commit

---
+++ Subscriptions/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Subscriptions
+//
+//  Created by Parth Patel on 08/09/20.
+//  Copyright © 2020 Parth Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
